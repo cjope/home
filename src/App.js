9f9f97bce84c224e59c33e76c973f4c42160df3f
Craig <craigjope@gmail.com>
2021-09-04 14:33:54 -0600

    everything working

--- src/App.js
+++ src/App.js
@@ -4,55 +4,6 @@ import './App.css'
 import Sidebar from './Sidebar'
 import Main from './Main'
 
-
-// function App() {
-
-//   const [notes, setNotes]=useState([])
-//   const [activeNote, setActiveNote]=useState(false)
-
-//   const onAddNote = () => {
-//     const newNote = {
-//       id: uuid(),
-//       title:"Untitled Note",
-//       body:"",
-//       lastModified:Date.now(),
-//     }
-//     setNotes([newNote, ...notes])
-//     setActiveNote(newNote.id)
-//   }
-
-//   const onDeleteNote = (idToDelete) => {
-//     setNotes(notes.filter((note)=> note.id !== idToDelete))
-//   }
-
-//   const getActiveNote = () => {
-//     return notes.find((note) => note.id === activeNote)
-//   }
-
-
-//   return (
-//     <div className="App">
-//     <Sidebar 
-//     notes={notes} 
-//     onAddNote={onAddNote} 
-//     onDeleteNote={onDeleteNote} 
-//     activeNote={activeNote} 
-//     setActiveNote={setActiveNote}/>
-
-//     <Main activeNote={getActiveNote()}/>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-// import { useEffect, useState } from "react";
-// import uuid from "react-uuid";
-// import "./App.css";
-// import Main from "./main/Main";
-// import Sidebar from "./sidebar/Sidebar";
-
 function App() {
   const [notes, setNotes] = useState(
     localStorage.notes ? JSON.parse(localStorage.notes) : []
