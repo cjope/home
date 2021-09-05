264cb31a7e58c21b5c85a0cc95572be0e9fcb6c3
Craig <craigjope@gmail.com>
2021-09-04 20:44:36 -0600

    Home.js created

--- src/App.js
+++ src/App.js
@@ -1,50 +1,51 @@
-import { useState, useEffect } from 'react'
-import uuid from 'react-uuid'
+// import { useState, useEffect } from 'react'
+// import uuid from 'react-uuid'
 import './App.css'
-import Sidebar from './Sidebar'
-import Main from './Main'
+// import Sidebar from './Sidebar'
+// import Main from './Main'
+import Home from './Home'
 
 function App() {
-  const [notes, setNotes] = useState(
-    localStorage.notes ? JSON.parse(localStorage.notes) : []
-  );
-  const [activeNote, setActiveNote] = useState(false);
+  // const [notes, setNotes] = useState(
+  //   localStorage.notes ? JSON.parse(localStorage.notes) : []
+  // );
+  // const [activeNote, setActiveNote] = useState(false);
 
-  useEffect(() => {
-    localStorage.setItem("notes", JSON.stringify(notes));
-  }, [notes]);
+  // useEffect(() => {
+  //   localStorage.setItem("notes", JSON.stringify(notes));
+  // }, [notes]);
 
-  const onAddNote = () => {
-    const newNote = {
-      id: uuid(),
-      title: "Untitled Note",
-      body: "",
-      lastModified: Date.now(),
-    };
+  // const onAddNote = () => {
+  //   const newNote = {
+  //     id: uuid(),
+  //     title: "Untitled Note",
+  //     body: "",
+  //     lastModified: Date.now(),
+  //   };
 
-    setNotes([newNote, ...notes]);
-    setActiveNote(newNote.id);
-  };
+  //   setNotes([newNote, ...notes]);
+  //   setActiveNote(newNote.id);
+  // };
 
-  const onDeleteNote = (noteId) => {
-    setNotes(notes.filter(({ id }) => id !== noteId));
-  };
+  // const onDeleteNote = (noteId) => {
+  //   setNotes(notes.filter(({ id }) => id !== noteId));
+  // };
 
-  const onUpdateNote = (updatedNote) => {
-    const updatedNotesArr = notes.map((note) => {
-      if (note.id === updatedNote.id) {
-        return updatedNote;
-      }
+  // const onUpdateNote = (updatedNote) => {
+  //   const updatedNotesArr = notes.map((note) => {
+  //     if (note.id === updatedNote.id) {
+  //       return updatedNote;
+  //     }
 
-      return note;
-    });
+  //     return note;
+  //   });
 
-    setNotes(updatedNotesArr);
-  };
+  //   setNotes(updatedNotesArr);
+  // };
 
-  const getActiveNote = () => {
-    return notes.find(({ id }) => id === activeNote);
-  };
+  // const getActiveNote = () => {
+  //   return notes.find(({ id }) => id === activeNote);
+  // };
 
   return (
     <div className="App">
